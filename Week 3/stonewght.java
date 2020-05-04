3ecc58b0ff7ec8fae3db1bfd4a9e90b52ab10958
Vrashabh <vrashabh.irde@carwow.co.uk>
2020-05-04 10:58:01 +0100

    Reminaing commits

---
+++ Week 3/stonewght.java
@@ -0,0 +1,50 @@
+/*
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+*/
+
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+         PriorityQueue<Integer> pq=
+                new PriorityQueue<Integer>(stones.length, (a,b) -> b - a);
+         for(int i: stones)
+         {
+             pq.add(i);
+         }
+        while(pq!=null && pq.size()>=0)
+        {
+            if(pq.size()==0)
+                return 0;
+            else if(pq.size()==1)
+                return pq.remove();
+            else
+            {
+            int x = pq.remove();
+            int y = pq.remove();
+            if(x!=y)
+                {
+                    pq.add(Math.abs(y-x)); 
+                 }
+            }
+        }
+        
+        return 0;
+        
+    }
+}
